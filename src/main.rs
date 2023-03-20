4bcc759b99ee1f08f58fe6a32f896b6233bf739b
f01dab1e <f01dab1e@protonmail.com>
2023-03-20 14:23:15 +0000

    simple cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use crate::ast::Expr;
+use crate::table::AllocTable;
+
 mod ast;
 mod parse;
 mod span;
 mod table;
 
-fn main() {}
+fn main() {
+    let path = std::env::args().nth(1).unwrap();
+    let text = std::fs::read_to_string(&path).unwrap();
+
+    let mut tables = AllocTable::default();
+    let ast: Vec<Expr> = parse::parse(&text, &mut tables).unwrap();
+
+    dbg!(ast);
+}
